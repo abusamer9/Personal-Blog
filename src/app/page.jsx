aa4ceeb23dabd6b2869fad1fea8158ddd2f556cd
Samer Aboobacker <sameraboobacker@Samers-MacBook-Air.local>
2024-11-30 13:11:21 -0500

    Changes for timeline

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
    <div className="p-4 justify-center">
-  Releasing soon
+  Releasing soon in December 2024
    </div>
   );
 }
